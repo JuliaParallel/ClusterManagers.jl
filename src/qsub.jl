fd59f851e1b70e41a0e26d07e260b2e8480eef7c
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-12 23:33:25 +0200

    Merge

--- src/qsub.jl
+++ src/qsub.jl
@@ -6,36 +6,6 @@ end
 immutable SGEManager <: ClusterManager
 end
 
-<<<<<<< HEAD
-function launch_qsub_workers(cman::Union(PBSManager, SGEManager), np::Integer, config::Dict)
-    exehome = config[:dir]
-    exename = config[:exename]
-    exeflags = config[:exeflags]
-    home = ENV["HOME"]
-    isPBS = isa(cman, PBSManager)
-    queue = haskey(config,:queue) ? string("-j ", config[:qeueu]) : ""
-
-    jobname = "julia-$(getpid())"
-    qsub_cmd = `echo "cd $(pwd()) && $exehome/$exename $exeflags"` |> (isPBS ? `qsub -N $jobname $queue -j oe -k o -t 1-$np` : `qsub -N $jobname $queue -terse -j y -t 1-$np`)
-    out,qsub_proc = readsfrom(qsub_cmd)
-    if !success(qsub_proc)
-        error("batch queue not available (could not run qsub)")
-    end
-    id = chomp(split(readline(out),'.')[1])
-    if endswith(id, "[]")
-        id = id[1:end-2]
-    end
-    filename(i) = isPBS ? "$home/$jobname-$i.o$id" : "$home/$jobname.o$id.$i"
-    print("job id is $id, waiting for job to start ")
-    io_objs = cell(np)
-    configs = cell(np)
-    for i=1:np
-        # wait for each output stream file to get created
-        fname = filename(i)
-        while !isfile(fname)
-            print(".")
-            sleep(1.0)
-=======
 function launch(manager::Union(PBSManager, SGEManager), np::Integer, config::Dict, instances_arr::Array, c::Condition)
     try
         exehome = config[:dir]
@@ -43,9 +13,10 @@ function launch(manager::Union(PBSManager, SGEManager), np::Integer, config::Dic
         exeflags = config[:exeflags]
         home = ENV["HOME"]
         isPBS = isa(manager, PBSManager)
+        queue = haskey(config,:queue) ? string("-j ", config[:qeueu]) : ""
 
         jobname = "julia-$(getpid())"
-        qsub_cmd = `echo "cd $(pwd()) && $exehome/$exename $exeflags"` |> (isPBS ? `qsub -N $jobname -j oe -k o -t 1-$np` : `qsub -N $jobname -terse -j y -t 1-$np`)
+        qsub_cmd = `echo "cd $(pwd()) && $exehome/$exename $exeflags"` |> (isPBS ? `qsub -N $jobname $queue -j oe -k o -t 1-$np` : `qsub -N $jobname $queue -terse -j y -t 1-$np`)
         out,qsub_proc = open(qsub_cmd)
         if !success(qsub_proc)
             println("batch queue not available (could not run qsub)")
@@ -72,7 +43,6 @@ function launch(manager::Union(PBSManager, SGEManager), np::Integer, config::Dic
             io_objs[i],io_proc = open(cmd)
             io_objs[i].line_buffered = true
             configs[i] = merge(config, {:job => id, :task => i, :iofile => fname, :process => io_proc})
->>>>>>> 8a476dc8b999a73f8fefaa9c457b7455bd6adc81
         end
         println("")
         
