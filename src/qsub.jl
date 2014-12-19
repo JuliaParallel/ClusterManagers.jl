5bcab6d931c34ac5e3847d4bc56de0c1c6dbd35e
Amit Murthy <amit.murthy@gmail.com>
2014-12-19 11:29:29 +0530

    track changes in Base

--- src/qsub.jl
+++ src/qsub.jl
@@ -1,18 +1,21 @@
 export PBSManager, SGEManager, addprocs_pbs, addprocs_sge
 
 immutable PBSManager <: ClusterManager
+    np::Integer
 end
 
 immutable SGEManager <: ClusterManager
+    np::Integer
 end
 
-function launch(manager::Union(PBSManager, SGEManager), np::Integer, config::Dict, instances_arr::Array, c::Condition)
+function launch(manager::Union(PBSManager, SGEManager), params::Dict, instances_arr::Array, c::Condition)
     try
-        exehome = config[:dir]
-        exename = config[:exename]
-        exeflags = config[:exeflags]
+        exehome = params[:dir]
+        exename = params[:exename]
+        exeflags = params[:exeflags]
         home = ENV["HOME"]
         isPBS = isa(manager, PBSManager)
+        np = manager.np
 
         jobname = "julia-$(getpid())"
         qsub_cmd = `echo "cd $(pwd()) && $exehome/$exename $exeflags"` |> (isPBS ? `qsub -N $jobname -j oe -k o -t 1-$np` : `qsub -N $jobname -terse -j y -t 1-$np`)
@@ -27,8 +30,6 @@ function launch(manager::Union(PBSManager, SGEManager), np::Integer, config::Dic
         end
         filename(i) = isPBS ? "$home/$jobname-$i.o$id" : "$home/$jobname.o$id.$i"
         print("job id is $id, waiting for job to start ")
-        io_objs = cell(np)
-        configs = cell(np)
         for i=1:np
             # wait for each output stream file to get created
             fname = filename(i)
@@ -39,25 +40,29 @@ function launch(manager::Union(PBSManager, SGEManager), np::Integer, config::Dic
             # Hack to get Base to get the host:port, the Julia process has already started.
             cmd = `tail -f $fname`
             cmd.detach = true
-            io_objs[i],io_proc = open(cmd)
-            io_objs[i].line_buffered = true
-            configs[i] = merge(config, {:job => id, :task => i, :iofile => fname, :process => io_proc})
+
+            config = WorkerConfig()
+
+            config.io, io_proc = open(cmd)
+            config.line_buffered = true
+
+            config.userdata = Dict{Symbol, Any}(:job => id, :task => i, :iofile => fname, :process => io_proc)
+            push!(instances_arr, config)
+            notify(c)
         end
         println("")
-        
-        push!(instances_arr, collect(zip(io_objs, configs)))
-        notify(c)
+
     catch e
         println("Error launching qsub")
         println(e)
     end
 end
 
-function manage(manager::Union(PBSManager, SGEManager), id::Integer, config::Dict, op::Symbol)
+function manage(manager::Union(PBSManager, SGEManager), id::Integer, config::WorkerConfig, op::Symbol)
     if op == :finalize
-        kill(config[:process])
-        if isfile(config[:iofile])
-            rm(config[:iofile])
+        kill(config.userdata[:process])
+        if isfile(config.userdata[:iofile])
+            rm(config.userdata[:iofile])
         end
 #     elseif op == :interrupt
 #         job = config[:job]
@@ -69,5 +74,5 @@ function manage(manager::Union(PBSManager, SGEManager), id::Integer, config::Dic
     end
 end
 
-addprocs_pbs(np::Integer) = addprocs(np, manager=PBSManager())
-addprocs_sge(np::Integer) = addprocs(np, manager=SGEManager())
+addprocs_pbs(np::Integer) = addprocs(PBSManager(np))
+addprocs_sge(np::Integer) = addprocs(SGEManager(np))
