00c54f7fb0f4bb41ea6f1924c64a368236025dab
Gustavo Camilo <gcamilo@gmail.com>
2015-10-15 11:31:24 -0400

    Fix Deprecation Warning

--- src/qsub.jl
+++ src/qsub.jl
@@ -27,7 +27,7 @@ function launch(manager::Union{PBSManager, SGEManager}, params::Dict, instances_
 
         jobname = "julia-$(getpid())"
         cmd = `cd $dir && $exename $exeflags`
-        qsub_cmd = `echo $(Base.shell_escape(cmd))` |> (isPBS ? `qsub -N $jobname $queue -j oe -k o -t 1-$np` : `qsub -N $jobname $queue -terse -j y -t 1-$np`)
+        qsub_cmd = pipeline(`echo $(Base.shell_escape(cmd))` , (isPBS ? `qsub -N $jobname $queue -j oe -k o -t 1-$np` : `qsub -N $jobname $queue -terse -j y -t 1-$np`))
         out,qsub_proc = open(qsub_cmd)
         if !success(qsub_proc)
             println("batch queue not available (could not run qsub)")
