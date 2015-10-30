9f954dfa266d7409d3de0f7129364e845f83ccff
Amit Murthy <amit.murthy@gmail.com>
2015-10-30 16:27:49 +0530

    workers connect stdout back to master

--- src/slurm.jl
+++ src/slurm.jl
@@ -17,6 +17,10 @@ function launch(manager::SlurmManager, params::Dict, instances_arr::Array,
         p = delete!(p, :exename)
         p = delete!(p, :exeflags)
         p = delete!(p, :topology)
+
+        capture_stdout = get(p, :capture_stdout, true)
+        p = delete!(p, :capture_stdout)
+
         srunargs = []
         for k in keys(p)
             if length(string(k)) == 1
@@ -36,41 +40,71 @@ function launch(manager::SlurmManager, params::Dict, instances_arr::Array,
             end
         end
 
+        stdout_ios = Channel(typemax(Int))
+        port = 0
+        if capture_stdout
+            # Start a listener for capturing stdout's from the workers
+            (port, server) = listenany(11000)
+            @schedule begin
+                nw = np
+                while nw > 0
+                    sock = accept(server)
+                    put!(stdout_ios, sock)
+                    nw = nw - 1
+                end
+            end
+        end
+
         # cleanup old files
         map(rm, filter(t -> ismatch(r"job.*\.out", t), readdir(exehome)))
 
         np = manager.np
         jobname = "julia-$(getpid())"
-        srun_cmd = `srun -J $jobname -n $np -o "job%4t.out" -D $exehome $(srunargs) $exename $exeflags --worker`
+        srun_cmd = `srun -J $jobname -n $np -o "job%4t.out" -D $exehome $(srunargs) $exename $exeflags`
+        if capture_stdout
+            host = getipaddr().host
+            exec_cmds = "c=connect(IPv4(host), port);Base.wait_connected(c);redirect_stdout(c);redirect_stderr(c);Base.start_worker(c)"
+            srun_cmd = `$srun_cmd $exec_cmds`
+        else
+            srun_cmd = `$srun_cmd --worker`
+        end
         out, srun_proc = open(srun_cmd)
+
         for i = 0:np - 1
-            print("connecting to worker $(i + 1) out of $np\r")
-            local w=[]
-            fn = "$exehome/job$(lpad(i, 4, "0")).out"
-            t0 = time()
-            while true
-                if time() > t0 + 60 + np
-                    warn("dropping worker: file not created in $(60 + np) seconds")
-                    break
-                end
-                sleep(0.001)
-                if isfile(fn) && filesize(fn) > 0
-                    w = open(fn) do f
-                        return split(split(readline(f), ":")[2], "#")
+            config = WorkerConfig()
+            # Keep a reference to the proc, so it's properly closed once
+            # the last worker exits.
+            config.userdata = srun_proc
+            if capture_stdout
+                config.io = take!(stdout_ios)
+            else
+                print("connecting to worker $(i + 1) out of $np\r")
+                local w=[]
+                fn = "$exehome/job$(lpad(i, 4, "0")).out"
+                t0 = time()
+                while true
+                    if time() > t0 + 60 + np
+                        warn("dropping worker: file not created in $(60 + np) seconds")
+                        break
                     end
-                    break
+                    sleep(0.001)
+                    if isfile(fn) && filesize(fn) > 0
+                        config.io = open(fn)
+                        w = open(fn) do f
+                            return split(split(readline(f), ":")[2], "#")
+                        end
+                        break
+                    end
+                end
+                if length(w) > 0
+                    config.port = parse(Int, w[1])
+                    config.host = strip(w[2])
+                else
+                    continue
                 end
             end
-            if length(w) > 0
-                config = WorkerConfig()
-                config.port = parse(Int, w[1])
-                config.host = strip(w[2])
-                # Keep a reference to the proc, so it's properly closed once
-                # the last worker exits.
-                config.userdata = srun_proc
-                push!(instances_arr, config)
-                notify(c)
-            end
+            push!(instances_arr, config)
+            notify(c)
         end
     catch e
         println("Error launching Slurm job:")
