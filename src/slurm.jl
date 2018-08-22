4efd227cb37d800bb5a47e5f851fe96992cd486f
Antoine R. Baldassari <baldassa@longleaf-login2.its.unc.edu>
2018-08-22 18:06:25 -0400

    minor 0.7+ fixes and updates to slurm.jl
    Fixes SLURM functionalities by (1) updating some deprecated syntax in slurm.jl, and (2) replacing broken cluster_cookie() call. Also adresses filter() deprecation warning

--- src/slurm.jl
+++ src/slurm.jl
@@ -6,6 +6,8 @@ struct SlurmManager <: ClusterManager
     np::Integer
 end
 
+slurm_worker_arg() = `--worker=$(Distributed.init_multi(); Distributed.LPROC.cookie)`
+
 function launch(manager::SlurmManager, params::Dict, instances_arr::Array,
                 c::Condition)
     try
@@ -13,8 +15,8 @@ function launch(manager::SlurmManager, params::Dict, instances_arr::Array,
         exename = params[:exename]
         exeflags = params[:exeflags]
 
-        stdkeys = keys(Base.Distributed.default_addprocs_params())
-        p = filter((x,y) -> !(x in stdkeys), params)
+        stdkeys = keys(Distributed.default_addprocs_params())
+        p = filter(x->!(x[1] in stdkeys), params)
 
         srunargs = []
         for k in keys(p)
@@ -40,8 +42,8 @@ function launch(manager::SlurmManager, params::Dict, instances_arr::Array,
 
         np = manager.np
         jobname = "julia-$(getpid())"
-        srun_cmd = `srun -J $jobname -n $np -o "job%4t.out" -D $exehome $(srunargs) $exename $exeflags $(worker_arg())`
-        out, srun_proc = open(srun_cmd)
+        srun_cmd = `srun -J $jobname -n $np -o "job%4t.out" -D $exehome $(srunargs) $exename $exeflags $(slurm_worker_arg())`
+        srun_proc = open(srun_cmd)
         for i = 0:np - 1
             print("connecting to worker $(i + 1) out of $np\r")
             local w=[]
