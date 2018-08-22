2b05b91690938b5eaf3041d64d47baea97f78267
Antoine R. Baldassari <baldassa@longleaf-login2.its.unc.edu>
2018-08-22 18:06:25 -0400

    minor 0.7+ fixes and updates to slurm.jl
    Fixes SLURM functionalities by (1) updating some deprecated syntax in slurm.jl, and (2) replacing broken cluster_cookie() call. Also adresses filter() deprecation warning

--- src/ClusterManagers.jl
+++ src/ClusterManagers.jl
@@ -6,7 +6,8 @@ using Sockets
 export launch, manage, kill, init_worker, connect
 import Distributed: launch, manage, kill, init_worker, connect
 
-worker_arg() = `--worker=$(cluster_cookie())`
+worker_arg() = `--worker=$(Distributed.init_multi(); cluster_cookie())`
+
 
 # PBS doesn't have the same semantics as SGE wrt to file accumulate,
 # a different solution will have to be found
