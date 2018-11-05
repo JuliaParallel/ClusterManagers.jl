440069b8845d52c2d3d92b03ffa407473e683b5d
Valentin Churavy <v.churavy@gmail.com>
2018-11-05 13:24:58 -0500

    add tests for LocalAffinityManager

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,14 @@
 # only package loading for now
 using ClusterManagers
+using Distributed
+using Test
+
+@testset "AffinityManager" begin
+    manager = addprocs(LocalAffinityManager(np = 2, mode = COMPACT, affinities = Int[]))
+    addprocs(manager)
+    rmprocs(manager)
+
+    manager = addprocs(LocalAffinityManager(np = 2, mode = BALANCED, affinities = Int[]))
+    addprocs(manager)
+    rmprocs(manager)
+end
