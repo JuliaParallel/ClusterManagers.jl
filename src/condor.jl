f91789be45336b0c4ca949ffd9853ba283cbccdf
Nick Amin <amin.nj@gmail.com>
2020-12-16 22:59:20 -0800

    add flexibility, allow remote connections

--- src/condor.jl
+++ src/condor.jl
@@ -10,6 +10,10 @@ function condor_script(portnum::Integer, np::Integer, params::Dict)
     dir = params[:dir]
     exename = params[:exename]
     exeflags = params[:exeflags]
+    extrajdl = get(params, :extrajdl, [])
+    extraenv = get(params, :extraenv, [])
+    extrainputs = get(params, :extrainputs, [])
+    telnetloc = get(params, :telnetloc, "/usr/bin/telnet")
     home = ENV["HOME"]
     hostname = ENV["HOSTNAME"]
     jobname = "julia-$(getpid())"
@@ -19,16 +23,24 @@ function condor_script(portnum::Integer, np::Integer, params::Dict)
     scriptf = open("$tdir/$jobname.sh", "w")
     println(scriptf, "#!/bin/sh")
     println(scriptf, "cd $(Base.shell_escape(dir))")
-    println(scriptf, "$(Base.shell_escape(exename)) $(Base.shell_escape(worker_arg())) | /usr/bin/telnet $(Base.shell_escape(hostname)) $portnum")
+    for line in extraenv
+        println(scriptf, line)
+    end
+    println(scriptf, "$(Base.shell_escape(exename)) $(Base.shell_escape(worker_arg())) | $telnetloc $(Base.shell_escape(hostname)) $portnum")
     close(scriptf)
 
+    input_files = ["$tdir/$jobname.sh"]
+    push!(input_files, extrainputs...)
     subf = open("$tdir/$jobname.sub", "w")
     println(subf, "executable = /bin/bash")
     println(subf, "arguments = ./$jobname.sh")
     println(subf, "universe = vanilla")
     println(subf, "should_transfer_files = yes")
-    println(subf, "transfer_input_files = $tdir/$jobname.sh")
+    println(subf, "transfer_input_files = $(join(input_files, ','))")
     println(subf, "Notification = Error")
+    for line in extrajdl
+        println(subf, line)
+    end
     for i = 1:np
         println(subf, "output = $tdir/$jobname-$i.o")
         println(subf, "error= $tdir/$jobname-$i.e")
@@ -42,16 +54,15 @@ end
 function launch(manager::HTCManager, params::Dict, instances_arr::Array, c::Condition)
     try
         portnum = rand(8000:9000)
-        server = listen(portnum)
+        _, server = listenany(Sockets.getaddrinfo("0.0.0.0"), portnum)
         np = manager.np
 
         script = condor_script(portnum, np, params)
-        out,proc = open(`condor_submit $script`)
-        if !success(proc)
+        cmd = `condor_submit $script`
+        if !success(cmd)
             println("batch queue not available (could not run condor_submit)")
             return
         end
-        print(readline(out))
         print("Waiting for $np workers: ")
 
         for i=1:np
