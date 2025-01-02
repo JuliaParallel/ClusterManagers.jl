fd8c985daa7dd7d6e11bff7bd0bef0b9e51d66a1
Dilum Aluthge <dilum@aluthge.com>
2025-01-02 02:26:48 -0500

    Update my_sbatch.sh

--- ci/my_sbatch.sh
+++ ci/my_sbatch.sh
@@ -3,4 +3,11 @@
 #SBATCH --ntasks=2
 #SBATCH --time=00:10:00
 
+# Important note:
+# There should be no non-comment non-whitespace lines above this line.
+
+set -euf -o pipefail
+
+set -x
+
 julia --project=ClusterManagers -e 'import Pkg; Pkg.test(; test_args=["slurm"])'
