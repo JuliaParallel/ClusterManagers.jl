dbd5ebe1cc4e7a139560ed33a3c99e6cee643b00
Dilum Aluthge <dilum@aluthge.com>
2025-01-02 01:46:34 -0500

    Create slurm.jl

---
+++ test/slurm.jl
@@ -0,0 +1,15 @@
+@testset "Slurm" begin
+    out_file = "my_slurm_job.out"
+    p = addprocs_slurm(1; o=out_file)
+    @test nprocs() == 2
+    @test workers() == p
+    @test fetch(@spawnat :any myid()) == p[1]
+    @test remotecall_fetch(+,p[1],1,1) == 2
+    rmprocs(p)
+    @test nprocs() == 1
+    @test workers() == [1]
+    
+    # Check output file creation
+    @test isfile(out_file)
+    rm(out_file)
+end
