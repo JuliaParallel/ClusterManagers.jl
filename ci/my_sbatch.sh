8819705d9070bdf63ab885926012f6d8d97875de
Dilum Aluthge <dilum@aluthge.com>
2025-01-02 02:25:54 -0500

    Update my_sbatch.sh

--- ci/my_sbatch.sh
+++ ci/my_sbatch.sh
@@ -3,4 +3,4 @@
 #SBATCH --ntasks=2
 #SBATCH --time=00:10:00
 
-julia --project=ClusterManagers -e 'using Pkg; Pkg.test(; test_args=["slurm"])'
+julia --project=ClusterManagers -e 'import Pkg; Pkg.test(; test_args=["slurm"])'
