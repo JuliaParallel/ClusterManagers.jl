0419748d2dd3b5654ad96eb503187dd9ed18566b
Dilum Aluthge <dilum@aluthge.com>
2025-02-09 18:53:56 -0500

    Test suite: Add tests for `show(io, ::ElasticManager)`

--- test/elastic.jl
+++ test/elastic.jl
@@ -12,4 +12,14 @@
     end
 
     wait(rmprocs(workers()))
+
+    @testset "show(io, ::ElasticManager)" begin
+        str = sprint(show, em)
+        lines = strip.(split(strip(str), '\n'))
+        @test lines[1] == "ElasticManager:"
+        @test lines[2] == "Active workers : []"
+        @test lines[3] == "Number of workers to be added  : 0"
+        @test lines[4] == "Terminated workers : [ 2]"
+        @test lines[5] == "Worker connect command :"
+    end
 end
